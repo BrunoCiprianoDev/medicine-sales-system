2f4d5bbd078bc4d22a759a75a797de5903b98ce0
PatoPiratah <iurifernandes1@hotmail.com>
2022-12-30 15:59:22 -0300

    estoque

--- application/src/pages/estoque/listEstoque/ListEstoque.js
+++ application/src/pages/estoque/listEstoque/ListEstoque.js
@@ -3,13 +3,14 @@ import styles from './ListEstoque.module.css'
 import Loading from '../../../components/loading/Loading'
 import PaginationComponent from '../../../components/paginationComponent/PaginationComponent'
 import { useState } from 'react'
-import {useSearchParams } from 'react-router-dom';
+import {useNavigate, useSearchParams } from 'react-router-dom';
 import { urlServer } from '../../../serverConfig'
 import { useFetch } from '../../../hooks/useFetch'
 import CircleBar from '../../../components/circleBar/CircleBar'
 
 
 const ListEstoque = ({filter}) => {
+const navigate = useNavigate();
 
 const url = urlServer+"/mercadorias/";
 
@@ -18,20 +19,35 @@ const parameters = [
         {id: 2, attribute:'codigo', label:'Código'},
         {id: 3, attribute:'estoque_minimo', label:'Estoque minimo'},
         {id: 4, attribute:'estoque_maximo', label:'Estoque máximo'},
-        {id: 5, attribute:'estoque_atual', label: 'Estoque atual'}
+        {id: 5, attribute:'estoque_atual', label: 'Estoque atual'},
+        {id: 6, attribute: 'nivel', label: 'Status'}
 ]
 
 let [searchParams] = useSearchParams();
 const {data, loading, error} = useFetch(
 filter ? url+"?"+searchParams : url)
 
+
+const totalUnidades = (lotes) => {
+  var unidades = 0;
+  lotes && lotes.map((lote)=>(
+    unidades+=lote.unidades
+  ))
+  return unidades;
+}
+
+const handleDetail = (id) => {
+  navigate('/estoque/detail/'+id)
+}
+
   //Params from pagination
-  const [itensPerPage, setItemPerPage] = useState(8);
+  const [itensPerPage, setItemPerPage] = useState(7);
   const [currentPage, setCurrentPage] = useState(0);
   const pages = Math.ceil(data && data.length / itensPerPage);
   const startIndex = currentPage * itensPerPage;
   const endIndex = startIndex + itensPerPage;
   const currentItens = (data && data.slice(startIndex, endIndex));
+
 
   return (
     <div className={styles.MainContainer}>
@@ -39,20 +55,19 @@ filter ? url+"?"+searchParams : url)
             {parameters.map((parameter)=>(
                 <div key={parameter.id} className={styles.ElementData}>{parameter.label}</div>
             ))}
-            <div className={styles.ElementData}></div>
         </div>
           {error && <p>Falha ao carregar dados....</p>}
           {loading && <Loading/>}
           {currentItens && currentItens.map((item)=>(
-            <div className={styles.ListComponent} key={item.id}>
-              {parameters.map((parameter)=>(
-                <div key={parameter.id} className={styles.ElementData}>
-                  {item[parameter.attribute]}
-                </div>
-              ))}
+            <div className={styles.ListComponent} key={item.id} onClick={()=>handleDetail(item.id)}>
+              <div className={styles.ElementData}>{item.nome}</div>
+              <div className={styles.ElementData}>{item.codigo}</div>
+              <div className={styles.ElementData}>{item.estoque_minimo}</div>
+              <div className={styles.ElementData}>{item.estoque_maximo}</div>
+              <div className={styles.ElementData}>{totalUnidades(item.lotes)}</div>
             <div className={styles.ElementData}>
                 <CircleBar  
-                    percentage={(item.estoque_atual/item.estoque_maximo)*100} 
+                    percentage={(totalUnidades(item.lotes)/item.estoque_maximo)*100} 
                     circleWidth='58' 
                     paramRadius={25} 
                     profile={7} 
