6b4e0f76e7f0c0b096da1901b7a483da73ad78d2
PatoPiratah <iurifernandes1@hotmail.com>
2023-01-03 12:25:47 -0300

    commit-ListSearch

--- application/src/components/listSearch/ListSearch.js
+++ application/src/components/listSearch/ListSearch.js
@@ -2,41 +2,41 @@ import React from 'react'
 import { useState } from 'react'
 import styles from './ListSearch.module.css'
 import { useFetch } from '../../hooks/useFetch'
+import ListSearchComponent from './ListSearchComponent'
+import AlertError from '../alertContainer/alertError/AlertError'
 
-const ListSearch = ({url, list, setList}) => {
-    const [inputSearch, setInputSearch] = useState('');
-    const {data, error} = useFetch(url+'?q='+inputSearch);
-    const currentItens = (data && data.slice(0, 14));
+const ListSearch = ({ url, list, setList }) => {
+  const [inputSearch, setInputSearch] = useState('');
+  const { data, error } = useFetch(url + '?q=' + inputSearch);
+  const currentItens = (data && data.slice(0, 5));
 
-    const handleList = (element) => {
-        if(!list.find(e=>e.id === element.id)){
-          setList(arr =>[...arr, {
-            id: element.id,
-            nome: element.nome,
-            valor_venda: element.valor_venda,
-            codigo: element.codigo,  
-            quant: 1,
-            quant_edit: 1
-          }]);
-        } 
-      }
-    
-    return (
+  const handleList = (element) => {
+    if (!list.find(e => e.id === element.id)) {
+      setList(arr => [...arr, {
+        id: element.id,
+        nome: element.nome,
+        valor_venda: element.valor_venda,
+        codigo: element.codigo,
+        quant: 1,
+        quant_edit: 1
+      }]);
+    }
+  }
+
+  return (
     <div className={styles.SearchList}>
-        <form >
-         <input onChange={(e)=>setInputSearch(e.target.value)} type="text" placeholder='Insira informações sobre a mercadoria'/>
-        </form>
-        <div className={styles.HeaderList}>
-          <label>Nome</label>
-          <label>Preço de venda</label>
-        </div>
-          {error && <h4>Falha ao carregar dados...</h4>}
-          {currentItens && currentItens.map((item)=>(
-            <div key={item.id} className={styles.ElementList} onClick={()=>handleList(item)}>
-              <label>{item.nome}</label>
-              <label>{item.valor_venda}</label>
-            </div>
-          ))}
+      <form >
+        <input onChange={(e) => setInputSearch(e.target.value)} type="text" placeholder='Insira informações sobre a mercadoria' />
+      </form>
+      <div className={styles.HeaderList}>
+        <label>Nome</label>
+        <label>Preço de venda</label>
+        <label>Unidades disponiveis</label>
+      </div>
+      {error && <AlertError>Erro no carregamento!</AlertError>}
+      {currentItens && currentItens.map((item) => (
+        <ListSearchComponent handleList={handleList} item={item} key={item.id}/>
+      ))}
     </div>
   )
 }
