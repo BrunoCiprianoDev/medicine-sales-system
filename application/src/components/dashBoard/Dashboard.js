eed00a9ccc78cbde4739abc67070053ad9c128cc
Whayan <88903707+Whayan@users.noreply.github.com>
2023-01-06 21:35:07 -0300

    06/01
    Dashboard

---
+++ application/src/components/dashBoard/Dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Dashboard.module.css'
+
+
+const Dashboard = () => {
+
+  return (
+    <div className={styles.MainContainer}>
+        <div className={styles.PainelSuperior}>
+        </div>
+        <div className={styles.PainelInferior}>
+
+        </div>
+        <div className={styles.PainelLateral}>
+        </div>
+    </div>
+  )
+}
+
+export default Dashboard
